3886c0afd7fc10bed2081cb149fa024846ab4438
Filipe Frota <filipeepedro123@gmail.com>
2025-12-03 11:58:43 +0000

    feat: Configuração inicial do Flask

---
+++ app.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+from flask import render_template, Flask
+import db
+
+APP = Flask(__name__)
+
+# Start page
+@APP.route('/')
+def index():
+    # TODO
+    return render_template('index.html',message='Hello World!')
