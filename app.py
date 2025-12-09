f3fcbc104c18bd674edec5b7ec199bb9c0abe658
Filipe Frota <filipeepedro123@gmail.com>
2025-12-09 23:50:46 +0000

    Adição dos endpoints cerimonia, categoria

--- app.py
+++ app.py
@@ -23,11 +23,49 @@ def cerimonias():
             GROUP BY cer.ano
         )
 
-        SELECT cer.cerimonia_id as cerimonia, cer.ano, COUNT(ca.categoria) as categorias, t1.nomeacoes as nomeacoes
+        SELECT cer.cerimonia_id, cer.ano, COUNT(ca.categoria) as categorias, t1.nomeacoes as nomeacoes
         FROM cerimonia as cer
         JOIN categoria_ano as ca on ca.cerimonia_id = cer.cerimonia_id
         JOIN t1 on t1.ano = cer.ano
         GROUP BY cer.cerimonia_id, cer.ano 
+        ORDER BY cer.cerimonia_id DESC
         """
     ).fetchall()
     return render_template('/cerimonias.html', cerimonias=cerimonias)
+
+@APP.route('/cerimonia/<int:id>/')
+def cerimonia(id):
+    cerimonia = db.execute(
+        """
+        SELECT cer.cerimonia_id, cer.ano, ca.categoria, ne.nome, ca.categoria_ano_id
+        FROM cerimonia AS cer
+        JOIN categoria_ano AS ca ON ca.cerimonia_id = cer.cerimonia_id
+        JOIN nomeacao AS n ON n.categoria_ano_id = ca.categoria_ano_id
+        JOIN concorre AS conc ON conc.nomeacao_id = n.nomeacao_id
+        JOIN nomeado AS ne ON ne.nomeado_id = conc.nomeado_id
+        WHERE n.ganhou = '1.0'
+        AND cer.cerimonia_id = ?
+        """
+        , (id,)
+    ).fetchall()
+    ano = cerimonia[0]['ano']
+    return render_template('cerimonia.html', cerimonia=cerimonia, id=id, ano=ano )
+
+@APP.route('/cerimonia/categoria/<int:id>')
+def categoria(id):
+    nomeacoes = db.execute(
+        """
+        SELECT cat.categoria_canonica, ca.categoria, ne.nome, ca.categoria_ano_id, cer.ano, cer.cerimonia_id
+        FROM cerimonia as cer
+        JOIN categoria_ano as ca on ca.cerimonia_id = cer.cerimonia_id
+        JOIN categoria as cat on cat.categoria_id = ca.categoria_id
+        JOIN nomeacao as n on n.categoria_ano_id = ca.categoria_ano_id
+        JOIN concorre as conc on conc.nomeacao_id = n.nomeacao_id
+        JOIN nomeado as ne on ne.nomeado_id = conc.nomeado_id
+        WHERE ca.categoria_ano_id = ?
+        """
+        , (id,)
+    ).fetchall()
+    ganhador = nomeacoes[0]
+    return render_template('categoria.html', ganhador=ganhador, nomeacoes=nomeacoes)
+
