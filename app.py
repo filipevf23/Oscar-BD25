cb6b92941fe2e08a4a86d574326c951748004bc3
Filipe Frota <filipeepedro123@gmail.com>
2025-12-10 00:14:56 +0000

    Nomeado agora aparece junto com o projeto que participou

--- app.py
+++ app.py
@@ -37,10 +37,11 @@ def cerimonias():
 def cerimonia(id):
     cerimonia = db.execute(
         """
-        SELECT cer.cerimonia_id, cer.ano, ca.categoria, ne.nome, ca.categoria_ano_id
+        SELECT cer.cerimonia_id, cer.ano, ca.categoria, f.nome AS filme_nome, ne.nome, ca.categoria_ano_id
         FROM cerimonia AS cer
         JOIN categoria_ano AS ca ON ca.cerimonia_id = cer.cerimonia_id
         JOIN nomeacao AS n ON n.categoria_ano_id = ca.categoria_ano_id
+        JOIN filme AS f ON f.filme_id = n.filme_id
         JOIN concorre AS conc ON conc.nomeacao_id = n.nomeacao_id
         JOIN nomeado AS ne ON ne.nomeado_id = conc.nomeado_id
         WHERE n.ganhou = '1.0'
@@ -55,14 +56,16 @@ def cerimonia(id):
 def categoria(id):
     nomeacoes = db.execute(
         """
-        SELECT cat.categoria_canonica, ca.categoria, ne.nome, ca.categoria_ano_id, cer.ano, cer.cerimonia_id
+        SELECT cat.categoria_canonica, ca.categoria, f.nome AS filme_nome, ne.nome, ca.categoria_ano_id, cer.ano, cer.cerimonia_id
         FROM cerimonia as cer
         JOIN categoria_ano as ca on ca.cerimonia_id = cer.cerimonia_id
         JOIN categoria as cat on cat.categoria_id = ca.categoria_id
         JOIN nomeacao as n on n.categoria_ano_id = ca.categoria_ano_id
+        JOIN filme as f on f.filme_id = n.filme_id
         JOIN concorre as conc on conc.nomeacao_id = n.nomeacao_id
         JOIN nomeado as ne on ne.nomeado_id = conc.nomeado_id
         WHERE ca.categoria_ano_id = ?
+        ORDER BY n.ganhou
         """
         , (id,)
     ).fetchall()
